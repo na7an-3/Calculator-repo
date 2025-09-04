cdd5aa273b0697f90714542893f88783420f97e8
nayan <paudelnayan765@gmail.com>
2025-09-04 21:53:53 +0545

    preparing the display

---
+++ index.js
@@ -0,0 +1,8 @@
+let calculator = "";
+let display = document.getElementById("display");
+
+const remd = document.getElementById("%");
+const div = document.getElementById("/");
+const mul = document.getElementById("*");
+const sub = document.getElementById("-");
+const add = document.getElementById("+");
